e142cdf3f96e0d9203898d6d766d3767b7fc0daa
Rohit Varshney <136239562+Rohit-ekd@users.noreply.github.com>
2026-07-19 15:04:07 +0530

    Refactor API base URL in api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://budget-tracker-92un.onrender.com/api",
+  baseURL: "https://budget-tracker-92un.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
